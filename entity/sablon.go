0c4b570ee273c40a85c85334ca223cadae1342a1
Ganes556 <gustiganes@gmail.com>
2024-03-04 21:16:53 +0800

    fix: there 2 id

--- entity/sablon.go
+++ entity/sablon.go
@@ -2,7 +2,6 @@ package entity
 
 type Sablon struct {
 	Base
-	ID            string          `gorm:"type:varchar(26);primaryKey;index:idx_sablon_id;not null" json:"id"`
 	Nama          string          `gorm:"type:varchar(150);unique;not null" json:"nama"`
 	Harga         float64         `gorm:"type:decimal(10,2);not null" json:"harga"`
 	DetailInvoice []DetailInvoice `gorm:"foreignKey:SablonID;references:ID" json:"detail_invoice,omitempty"`
